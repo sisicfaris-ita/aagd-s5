1d4018f95321a0d3e7db0e79c1ed401c9c6d8d73
Administrator <administrator@Administrators-iMac-2.local>
2024-06-27 19:57:02 +0200

    chore: react app cleanup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return <div className="h-full w-full flex flex-col gap-2 justify-center items-center">
+    <span className="text-4xl font-bold">Animation and Game Development</span>
+    <span className="text-3xl">Session 5</span>
+  </div>
+}
+
+export default App;
